94da7acef98313765d2a0e90a68990828cc66ac6
Jort van Gorkum <jortvangorkum@gmail.com>
2022-06-03 17:29:39 +0200

    Add Material UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import theme from './theme'
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <CssBaseline enableColorScheme />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
---
+++ src/theme.tsx
@@ -0,0 +1,5 @@
+import { createTheme } from "@mui/system";
+
+const theme = createTheme({})
+
+export default theme;
