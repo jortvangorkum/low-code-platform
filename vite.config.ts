40ceffc06c95ee1e34dd2b0f8defe5584a15de27
Jort van Gorkum <jortvangorkum@gmail.com>
2022-06-03 16:41:14 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react({
+    jsxImportSource: "@emotion/react",
+    babel: {
+      presets: ["@emotion/babel-preset-css-prop"],
+      plugins: ["@emotion/babel-plugin"]
+    }
+  })]
+})
