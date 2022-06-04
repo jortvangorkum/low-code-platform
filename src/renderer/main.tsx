c651b99ae147d50bdda6aefe31776c4c87b1cfd3
Jort van Gorkum <jortvangorkum@gmail.com>
2022-06-04 23:20:20 +0200

    Added roboto font package and fixed unsafe eval error

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,8 +1,14 @@
-import { CssBaseline, ThemeProvider } from '@mui/material'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import theme from './theme'
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import theme from './theme';
+
+// Import fonts
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -11,7 +17,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
-)
-
-console.log("GOT HERE")
+  </React.StrictMode>,
+);
--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,7 +1,13 @@
 const rules = require('./webpack.rules');
 const plugins = require('./webpack.plugins');
 
+rules.push({
+  test: /\.css$/i,
+  use: ["style-loader", "css-loader"],
+});
+
 module.exports = {
+  devtool: 'source-map',
   module: {
     rules,
   },
