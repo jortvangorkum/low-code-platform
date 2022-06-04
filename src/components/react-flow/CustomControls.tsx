4db28e662249b5277e5e3585d9e78b5e645b5e03
Jort van Gorkum <jortvangorkum@gmail.com>
2022-06-04 17:10:20 +0200

    Add ts-morph

--- src/components/react-flow/CustomControls.tsx
+++ src/components/react-flow/CustomControls.tsx
@@ -1,12 +1,19 @@
-import { ControlButton, Controls } from "react-flow-renderer";
+import { ControlButton, Controls, useNodes } from 'react-flow-renderer';
 import { PlayArrow } from '@mui/icons-material';
+import generateTypescript from '../../compile';
+import { NodeDataTypes } from './NodeTypes';
 
 export default function CustomControls() {
   return (
     <Controls>
-      <ControlButton onClick={() => console.log('action')}>
+      <ControlButton
+        onClick={() => {
+          const nodes = useNodes<NodeDataTypes>();
+          generateTypescript(nodes);
+        }}
+      >
         <PlayArrow />
       </ControlButton>
     </Controls>
-  )
+  );
 }
